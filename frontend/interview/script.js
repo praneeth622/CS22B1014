43829f4fb257d89f86a9c239719c12722d0883c5
Devarasetty Praneeth <praneethdevarasetty31@gmail.com>
2025-04-25 14:28:51 +0530

    interview

---
+++ frontend/interview/script.js
@@ -0,0 +1,16 @@
+
+const temp  = document.getElementById
+const forunit = document.getElementsByClassName(".dropdown")
+
+const tounit = document.getElementsByClassName("")
+
+
+function convertTemp(){
+    const value = document.getElementsByClassName("tnum")
+    const temp = parseInt(value)
+
+    const funit = document.getElementsByClassName("funit");
+    const result = document.getElementsByClassName("result")
+
+    
+}
